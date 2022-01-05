abbb8b1cde5731c3ad85b6bd0259adc0ed47204e
Ismail-Lh <ismailsaad03@gmail.com>
2022-01-05 17:49:33 +0100

    Create the ProductDetails page

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -183,7 +183,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "66540dc822bc44a4"; }
+/******/ 		__webpack_require__.h = function() { return "b04aec619ee178cf"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- frontend/screens/ProductScreen.jsx
+++ frontend/screens/ProductScreen.jsx
@@ -1,5 +1,79 @@
-const ProductScreen = () => {
-  return <div>ProductScreen</div>;
+import { NextLink } from '@components';
+import { Row, Col, Image, ListGroup, Card, Button } from 'react-bootstrap';
+
+import { Rating } from '@components';
+import products from '../products';
+
+const ProductScreen = ({ productId }) => {
+  const product = products.find(({ _id }) => _id === productId);
+
+  return (
+    <>
+      <NextLink route='/'>
+        <button className='btn btn-dark my-3'>Go Back</button>
+      </NextLink>
+
+      <Row>
+        <Col md={6}>
+          <Image src={product.image} alt={product.name} fluid />
+        </Col>
+
+        <Col md={3}>
+          <ListGroup variant='flush'>
+            <ListGroup.Item>
+              <h3>{product.name}</h3>
+            </ListGroup.Item>
+
+            <ListGroup.Item>
+              <Rating
+                value={product.rating}
+                text={`${product.numReviews} reviews`}
+              />
+            </ListGroup.Item>
+
+            <ListGroup.Item>Price: ${product.price}</ListGroup.Item>
+
+            <ListGroup.Item>Description:{product.description}</ListGroup.Item>
+          </ListGroup>
+        </Col>
+
+        <Col md={3}>
+          <Card>
+            <ListGroup variant='flush'>
+              <ListGroup.Item>
+                <Row>
+                  <Col>Price:</Col>
+                  <Col>
+                    <strong>${product.price}</strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Row>
+                  <Col>Status:</Col>
+                  <Col>
+                    <strong>
+                      {product.countInStock > 0 ? 'In Stock' : 'Out Of Stock'}
+                    </strong>
+                  </Col>
+                </Row>
+              </ListGroup.Item>
+
+              <ListGroup.Item>
+                <Button
+                  className='btn-block'
+                  type='button'
+                  disabled={product.countInStock === 0}>
+                  Add To Cart
+                </Button>
+              </ListGroup.Item>
+            </ListGroup>
+          </Card>
+        </Col>
+      </Row>
+    </>
+  );
 };
 
 export default ProductScreen;

f6da6feba82b23897138ae0055cbccf74a633ba7
Ismail-Lh <ismailsaad03@gmail.com>
2022-01-12 16:12:38 +0100

    Create a Cart reducer & add to cart action

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -7,12 +7,21 @@ import {
   productListReducer,
 } from './reducers/productReducers';
 
+import { cartReducer } from './reducers/cartReducers';
+
 const reducer = combineReducers({
   productList: productListReducer,
   productDetails: productDetailsReducer,
+  cart: cartReducer,
 });
 
-const initialState = {};
+const cartItemsFromStorage = localStorage.getItem('cartItems')
+  ? JSON.parse(localStorage.getItem('cartItems'))
+  : [];
+
+const initialState = {
+  cart: { cartItems: cartItemsFromStorage },
+};
 
 const middleware = [thunk];
 
